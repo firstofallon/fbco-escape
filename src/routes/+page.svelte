dc3d29f285f22a2012287c061116a7b16d11a7e3
Dale Cameron <fortiscontego@gmail.com>
2023-02-21 08:18:48 -0600

    updated main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>FBCO Escape Room</h1>
+<p>Stay tuned for something exciting!</p>
