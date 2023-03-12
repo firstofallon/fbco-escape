c89d72732e6ddb9e51171c8c7a5c3021b0f150c7
Dale Cameron <fortiscontego@gmail.com>
2023-03-12 15:08:50 -0500

    Prep for move to firebase

---
+++ src/routes/cms/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import CMS from '@staticcms/core';
+
+	
+	// Initialize the CMS object
+	CMS.init();
+	// Now the registry is available via the CMS object.
+	//CMS.registerPreviewTemplate('my-template', MyTemplate);
+</script>
